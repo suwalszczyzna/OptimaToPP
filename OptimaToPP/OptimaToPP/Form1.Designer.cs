3516bfb8b05ec94124a0ace6b9cd2f14d6a2eab6
Damian <damian.suwala@gmail.com>
2018-11-30 16:44:46 +0100

    Form improvements

--- OptimaToPP/OptimaToPP/Form1.Designer.cs
+++ OptimaToPP/OptimaToPP/Form1.Designer.cs
@@ -36,7 +36,6 @@
             this.XLSpath = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.linkLabel2 = new System.Windows.Forms.LinkLabel();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -45,9 +44,10 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(12, 541);
+            this.linkLabel1.Location = new System.Drawing.Point(9, 440);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(105, 17);
+            this.linkLabel1.Size = new System.Drawing.Size(83, 13);
             this.linkLabel1.TabIndex = 6;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Damian Suwała";
@@ -58,19 +58,22 @@
             this.groupBox1.Controls.Add(this.btnConvert);
             this.groupBox1.Controls.Add(this.btnOpenFile);
             this.groupBox1.Controls.Add(this.XLSpath);
-            this.groupBox1.Location = new System.Drawing.Point(13, 26);
+            this.groupBox1.Location = new System.Drawing.Point(10, 21);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(448, 130);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(336, 106);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Wskaż plik XLS";
+            this.groupBox1.Text = "Otwórz plik XLS";
             // 
             // btnConvert
             // 
             this.btnConvert.AutoSize = true;
-            this.btnConvert.Location = new System.Drawing.Point(6, 68);
+            this.btnConvert.Location = new System.Drawing.Point(4, 55);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnConvert.Name = "btnConvert";
-            this.btnConvert.Size = new System.Drawing.Size(423, 41);
+            this.btnConvert.Size = new System.Drawing.Size(317, 33);
             this.btnConvert.TabIndex = 2;
             this.btnConvert.Text = "Konwertuj";
             this.btnConvert.UseVisualStyleBackColor = true;
@@ -79,58 +82,55 @@
             // btnOpenFile
             // 
             this.btnOpenFile.AutoSize = true;
-            this.btnOpenFile.Location = new System.Drawing.Point(6, 21);
+            this.btnOpenFile.Location = new System.Drawing.Point(4, 17);
+            this.btnOpenFile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnOpenFile.Name = "btnOpenFile";
-            this.btnOpenFile.Size = new System.Drawing.Size(120, 41);
+            this.btnOpenFile.Size = new System.Drawing.Size(90, 33);
             this.btnOpenFile.TabIndex = 1;
-            this.btnOpenFile.Text = "Otwórz plik";
+            this.btnOpenFile.Text = "Przeglądaj...";
             this.btnOpenFile.UseVisualStyleBackColor = true;
             this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
             // 
             // XLSpath
             // 
-            this.XLSpath.Location = new System.Drawing.Point(149, 30);
+            this.XLSpath.Location = new System.Drawing.Point(112, 24);
+            this.XLSpath.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.XLSpath.Name = "XLSpath";
-            this.XLSpath.Size = new System.Drawing.Size(280, 22);
+            this.XLSpath.Size = new System.Drawing.Size(211, 20);
             this.XLSpath.TabIndex = 0;
             // 
             // groupBox2
             // 
             this.groupBox2.AutoSize = true;
             this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Location = new System.Drawing.Point(18, 159);
+            this.groupBox2.Location = new System.Drawing.Point(14, 129);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(448, 83);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(336, 67);
             this.groupBox2.TabIndex = 8;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Zapisz jako XLS dla Poczty Polskiej";
+            this.groupBox2.Text = "Eksport XLS dla Poczty Polskiej";
             // 
             // button1
             // 
             this.button1.AutoSize = true;
-            this.button1.Location = new System.Drawing.Point(6, 21);
+            this.button1.Location = new System.Drawing.Point(4, 17);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(423, 41);
+            this.button1.Size = new System.Drawing.Size(317, 33);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Eksportuj plik XLS";
+            this.button1.Text = "Zapisz jako";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.saveXLSforPP);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 255);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(154, 17);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "2018 - Damian Suwała ";
             // 
             // linkLabel2
             // 
             this.linkLabel2.AutoSize = true;
-            this.linkLabel2.Location = new System.Drawing.Point(283, 255);
+            this.linkLabel2.Location = new System.Drawing.Point(212, 207);
+            this.linkLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(178, 17);
+            this.linkLabel2.Size = new System.Drawing.Size(135, 13);
             this.linkLabel2.TabIndex = 10;
             this.linkLabel2.TabStop = true;
             this.linkLabel2.Text = "damian.suwala@gmail.com";
@@ -138,18 +138,18 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(478, 284);
+            this.ClientSize = new System.Drawing.Size(358, 231);
             this.Controls.Add(this.linkLabel2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.linkLabel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.HelpButton = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
@@ -170,7 +170,6 @@
         private System.Windows.Forms.TextBox XLSpath;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.LinkLabel linkLabel2;
     }
 }
